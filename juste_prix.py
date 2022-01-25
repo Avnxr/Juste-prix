8f6877a5c6787ac9bb65e136e4d7c415e0f54e92
KeziixTV <42646129+KeziixTV@users.noreply.github.com>
2022-01-25 16:35:23 +0100

    Update juste_prix.py

--- juste_prix.py
+++ juste_prix.py
@@ -1,37 +1,28 @@
 from random import randint
 
-def main():
 
-    # Statut de jeu
-    running = True
+def price():
 
-    # Choisir un nombre entre 1 et 1000
     just_price = randint(1, 100)
 
-    # Tant que le jeu n'est pas fini
+    running = True
 
     while running:
 
-        # Demander à l'utilisateur de trouver un prix
-        user_price = int(input("Entrez un prix : "))
-
-        # Si le prix est le meme que le juste prix
-        if user_price == just_price:
-            print("Trouvé ! Le juste prix était bien ", just_price)
-            # fin du jeu
+        user_price = int(input("Choississez un prix"))
+        
+        if(user_price == just_price):
+            print("Bravo ! Vous avez bien trouvé", just_price)
             running = False
 
-        # Si le prix de l'utilisateur est supérieur au prix à trouver
         elif user_price > just_price:
             print("C'est moins !")
 
-        # Si le prix de l'utilisateur est inférieur au prix à trouver
         elif user_price < just_price:
             print("C'est plus !")
 
-    # Fin du jeu après la boucle
-    print("Fin du jeu !")
+    print("Fin du jeu")
 
 
 if __name__ == '__main__':
-    main()
+    price()
